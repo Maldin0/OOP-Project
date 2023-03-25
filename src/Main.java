1dc0ab66d182016b4b211c149780268b6adcc419
Madino <65070015@kmitl.ac.th>
2023-03-25 16:15:29 +0700

    Test Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author Maldin0
+ * @created 3/25/2023 - 4:14 PM
+ * @project OOP-Project
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

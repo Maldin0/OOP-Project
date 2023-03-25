eca39f60d4335e8ea97f3d939815f421f4cbf5de
Laughingegg <112687944+Sneezzzzzze@users.noreply.github.com>
2023-03-25 21:00:19 +0700

    push my JB

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
-/**
- * @author Maldin0
- * @created 3/25/2023 - 4:14 PM
- * @project OOP-Project
- */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        System.out.println("Hello Gay");
     }
 }

25ae185c2cfc14ed1a29245391b80920bf21eeaf
Chirakit <65070034@kmitl.ac.th>
2023-04-06 22:53:42 +0700

    Create Credit.java
    coming soon

---
+++ src/Credit.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+public class Credit {
+    private JFrame fr;
+    private JLabel lb;
+    private JButton btn1;
+    private JDesktopPane desktopPane;
+    private JInternalFrame.JDesktopIcon icon1;
+
+    public Credit(){
+        JFrame fr = new JFrame();
+        JLabel lb = new JLabel();
+        JButton btn1 = new JButton();
+    }
+}

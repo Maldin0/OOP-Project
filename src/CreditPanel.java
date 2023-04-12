48563254c1d5e4603d61ac3d3b8ad20725e74f5c
Laughingegg <112687944+Sneezzzzzze@users.noreply.github.com>
2023-04-12 17:10:12 +0700

    Add font for Boss

--- src/CreditPanel.java
+++ src/CreditPanel.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class CreditPanel{
+public class CreditPanel {
     private JFrame fr;
     private JPanel panel;
     private JLabel name1, name2, name3, name4, name5, name6;
@@ -10,15 +10,24 @@ public class CreditPanel{
 
     public CreditPanel(){
         fr = new JFrame("Credit");
-        panel = new JPanel();
+        Font thaiFont = new Font("Tahoma", Font.BOLD, 14);
+
+        panel = new JPanel(new FlowLayout());
         name1 = new JLabel("นางสาวกนลรัตน์ เบิกขุนทด");
         name2 = new JLabel("นายก้องภพ สินสังข์");
         name3 = new JLabel("นายกิตติพล สิริอมรกิติ์");
         name4 = new JLabel("นายจิรกิตต์ มราชชาลี");
         name5 = new JLabel("นายจุนบอม คิม");
         name6 = new JLabel("นายชัยพิพัฒน์ พงษ์ประภาชื่น");
-        backBtn = new JButton("Back To Main Menu");
 
+        name1.setFont(thaiFont);
+        name2.setFont(thaiFont);
+        name3.setFont(thaiFont);
+        name4.setFont(thaiFont);
+        name5.setFont(thaiFont);
+        name6.setFont(thaiFont);
+
+        backBtn = new JButton("Back To Main Menu");
         panel.add(name1);
         panel.add(name2);
         panel.add(name3);
@@ -31,7 +40,7 @@ public class CreditPanel{
         fr.add(panel);
         fr.setLayout(new FlowLayout());
         fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        fr.setSize(500,500);
+        fr.setSize(new Dimension(800, 600));
         fr.setVisible(true);
     }
     public static void main(String[] args) {

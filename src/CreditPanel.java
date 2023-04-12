dac4ad0ce4b26d6364a2f74663a7c4554d764707
Chirakit <65070034@kmitl.ac.th>
2023-04-12 11:40:51 +0700

    Update CreditPanel.java

--- src/CreditPanel.java
+++ src/CreditPanel.java
@@ -9,7 +9,7 @@ public class CreditPanel{
     private JButton backBtn;
 
     public CreditPanel(){
-        fr = new JFrame();
+        fr = new JFrame("Credit");
         panel = new JPanel();
         name1 = new JLabel("นางสาวกนลรัตน์ เบิกขุนทด");
         name2 = new JLabel("นายก้องภพ สินสังข์");
@@ -19,14 +19,14 @@ public class CreditPanel{
         name6 = new JLabel("นายชัยพิพัฒน์ พงษ์ประภาชื่น");
         backBtn = new JButton("Back To Main Menu");
 
-        //panel.add(name1);
-        //panel.add(name2);
-        //panel.add(name3);
-        //panel.add(name4);
-        //panel.add(name5);
-        //panel.add(name6);
+        panel.add(name1);
+        panel.add(name2);
+        panel.add(name3);
+        panel.add(name4);
+        panel.add(name5);
+        panel.add(name6);
         panel.add(backBtn);
-        panel.setLayout(new FlowLayout());
+        panel.setLayout(new GridLayout(3,3));
 
         fr.add(panel);
         fr.setLayout(new FlowLayout());

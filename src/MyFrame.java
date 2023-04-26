b0c85a0585834228a0f6f05c537b6e0e226f93b7
Sweet eyes <109953598+Bonbonbonsai@users.noreply.github.com>
2023-04-26 22:40:33 +0700

    update pause button

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 public class MyFrame extends JFrame implements ActionListener{
-    private JButton btn1, btn2, btn3, btn4, btn5, btn6;
+    private JButton btn1, btn2, btn3, btn4, btn5, btn6, pauseButton;
     private JLabel label;
     private JPanel menuPanel;
     private boolean soundOn = true;
@@ -11,7 +11,7 @@ public class MyFrame extends JFrame implements ActionListener{
         this.getContentPane().setPreferredSize(new Dimension(800, 600));
 
         // Menu Panel
-        menuPanel = new JPanel() {
+        setMenuPanel(new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
@@ -22,59 +22,59 @@ public class MyFrame extends JFrame implements ActionListener{
                 Image slimeFront = new ImageIcon("image/Slime front.png").getImage();
                 g.drawImage(slimeFront, 225, 80, 600, 500, null);
             }
-        };
-        menuPanel.setLayout(null);
+        });
+        getMenuPanel().setLayout(null);
 
         // Start Button;
         ImageIcon button1 = new ImageIcon("image/Start button.png");
         btn1 = new JButton();
         btn1.setIcon(button1);
         btn1.setBounds(100, 290, 128, 64);
-        menuPanel.add(btn1);
+        getMenuPanel().add(btn1);
 
         // ScoreBoard;
         ImageIcon button2 = new ImageIcon("image/Scoreboard button.png");
         btn2 = new JButton();
         btn2.setIcon(button2);
         btn2.setBounds(100, 365, 128, 64);
-        menuPanel.add(btn2);
+        getMenuPanel().add(btn2);
 
         // Credit;
         ImageIcon button3 = new ImageIcon("image/Credit button.png");
         btn3 = new JButton();
         btn3.setIcon(button3);
         btn3.setBounds(100, 440, 128, 64);
-        menuPanel.add(btn3);
+        getMenuPanel().add(btn3);
 
         // Exit;
         ImageIcon button4 = new ImageIcon("image/Exit button.png");
         btn4 = new JButton();
         btn4.setIcon(button4);
         btn4.setBounds(100, 515, 128, 64);
-        menuPanel.add(btn4);
+        getMenuPanel().add(btn4);
 
         // How to Play;
         ImageIcon button5 = new ImageIcon("image/Help button.png");
         btn5 = new JButton();
         btn5.setIcon(button5);
         btn5.setBounds(10, 15, 50, 50);
-        menuPanel.add(btn5);
+        getMenuPanel().add(btn5);
 
         // Sound Setting;
         btn6 = new JButton(new ImageIcon("image/Sound button.png"));
         btn6.setBounds(80, 15, 50, 50);
-        menuPanel.add(btn6);
+        getMenuPanel().add(btn6);
 
         //  Logo
         label = new JLabel(new ImageIcon("image/Finished logo.png"));
         label.setBounds(40, 110, 250, 150);
-        menuPanel.add(label);
+        getMenuPanel().add(label);
 
         // Insert Icon Image
         ImageIcon icon2 = new ImageIcon("image/Slimeconnector_icon.png");
         this.setIconImage(icon2.getImage().getScaledInstance(5000, 5000, java.awt.Image.SCALE_SMOOTH));
         // Add menuPanel in JFrame
-        this.add(menuPanel);
+        this.add(getMenuPanel());
 
         // JFrame
         this.setTitle("Slime Connector");
@@ -95,9 +95,12 @@ public class MyFrame extends JFrame implements ActionListener{
     public void actionPerformed(ActionEvent e) {
         // if Click Start Button goes to GamePanel
         ch = new ChooseFrame();
+        pauseButton = new JButton("");
+        pauseButton.setBounds(10, 10, 50, 50);
         if (e.getSource().equals(btn1)) {
-            this.remove(menuPanel);
+            this.remove(getMenuPanel());
             this.revalidate();
+            this.add(pauseButton);
             if (ch.getX() == 1) {
                 new GamePanel(4, 4);
             }
@@ -122,5 +125,13 @@ public class MyFrame extends JFrame implements ActionListener{
     }
     public static void main(String[] args) {
         new MyFrame();
+    }
+
+    public JPanel getMenuPanel() {
+        return menuPanel;
+    }
+
+    public void setMenuPanel(JPanel menuPanel) {
+        this.menuPanel = menuPanel;
     }
 }

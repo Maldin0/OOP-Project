fc44fd58c1807134447e5389a9f4859e44232ee5
Sweet eyes <109953598+Bonbonbonsai@users.noreply.github.com>
2023-05-05 21:24:32 +0700

    แยก event ออกจาก ChoosePanel

---
+++ src/ChoosePanelEvent.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ChoosePanelEvent implements ActionListener {
+    private ChoosePanel choosePanel;
+    public ChoosePanelEvent(ChoosePanel choosePanel) {
+        this.choosePanel = choosePanel;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource().equals(choosePanel.getEasy())) {
+            JOptionPane.getRootFrame().dispose();
+            choosePanel.difficulty = 1;
+        }
+        else if (e.getSource().equals(choosePanel.getNormal())) {
+            JOptionPane.getRootFrame().dispose();
+            choosePanel.difficulty = 2;
+        }
+        else if (e.getSource().equals(choosePanel.getHard())) {
+            JOptionPane.getRootFrame().dispose();
+            choosePanel.difficulty = 3;
+        }
+    }
+}

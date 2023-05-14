f869ff0ac846cd6052feed5675051db88c615b8c
Sweet eyes <109953598+Bonbonbonsai@users.noreply.github.com>
2023-05-14 15:56:54 +0700

    PausePanel
    สร้าง event แยกกับ gui

--- src/PausePanel.java
+++ src/PausePanel.java
@@ -1,12 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
 public class PausePanel {
-    private MyFrameEvent myFrameEvent;
     private MyFrame myFrame;
     private JButton con, retry, exits;
+    private JDialog dialog;
+    private PausePanelEvent pausePanelEvent;
     public PausePanel(MyFrame mf) {
         this.myFrame = mf;
-        myFrameEvent = new MyFrameEvent(myFrame);
+
         con = new JButton(new ImageIcon("image/Continue button.png"));
         retry = new JButton(new ImageIcon("image/Restart button.png"));
         exits = new JButton(new ImageIcon("image/Exit to menu button.png"));
@@ -15,47 +16,10 @@ public class PausePanel {
         retry.setPreferredSize(new Dimension(128, 64));
         exits.setPreferredSize(new Dimension(192, 64));
 
-        con.addActionListener(e -> {
-            JOptionPane.getRootFrame().dispose();
-        });
-
-        retry.addActionListener(e -> {
-            int check = myFrame.getCheck();
-
-            if (check == 0) {
-                myFrame.getContentPane().removeAll();
-                myFrame.revalidate();
-                myFrame.add(myFrame.getPauseButton());
-                myFrame.add(new GamePanel(4, 4));
-                System.out.println(check);
-            }
-            else if (check == 1) {
-                myFrame.getContentPane().removeAll();
-                myFrame.revalidate();
-                myFrame.add(myFrame.getPauseButton());
-                myFrame.add(new GamePanel(6, 6));
-                System.out.println(check);
-            }
-            else if (check == 2) {
-                myFrame.getContentPane().removeAll();
-                myFrame.revalidate();
-                myFrame.add(myFrame.getPauseButton());
-                myFrame.add(new GamePanel(8, 8));
-                System.out.println(check);
-            }
-            JOptionPane.getRootFrame().dispose();
-            myFrame.repaint();
-            myFrame.pack();
-        });
-
-        exits.addActionListener(e -> {
-            myFrame.getContentPane().removeAll();
-            myFrame.revalidate();
-            myFrame.add(myFrame.getMenuPanel());
-            JOptionPane.getRootFrame().dispose();
-            myFrame.repaint();
-            myFrame.pack();
-        });
+        pausePanelEvent = new PausePanelEvent(this, myFrame);
+        con.addActionListener(pausePanelEvent);
+        retry.addActionListener(pausePanelEvent);
+        exits.addActionListener(pausePanelEvent);
 
         JLabel label = new JLabel("Pause");
 
@@ -71,7 +35,7 @@ public class PausePanel {
         mainPanel.add(buttonPanel);
 
 
-        JDialog dialog = new JDialog();
+        dialog = new JDialog();
         dialog.setTitle("Pausssssse");
         dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
         dialog.setUndecorated(false);
@@ -82,5 +46,45 @@ public class PausePanel {
         dialog.setLocationRelativeTo(null);
         dialog.setVisible(true);
 
+    }
+
+    public MyFrame getMyFrame() {
+        return myFrame;
+    }
+
+    public void setMyFrame(MyFrame myFrame) {
+        this.myFrame = myFrame;
+    }
+
+    public JButton getCon() {
+        return con;
+    }
+
+    public void setCon(JButton con) {
+        this.con = con;
+    }
+
+    public JButton getRetry() {
+        return retry;
+    }
+
+    public void setRetry(JButton retry) {
+        this.retry = retry;
+    }
+
+    public JButton getExits() {
+        return exits;
+    }
+
+    public void setExits(JButton exits) {
+        this.exits = exits;
+    }
+
+    public JDialog getDialog() {
+        return dialog;
+    }
+
+    public void setDialog(JDialog dialog) {
+        this.dialog = dialog;
     }
 }
---
+++ src/PausePanelEvent.java
@@ -0,0 +1,55 @@
+import javax.swing.JOptionPane;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PausePanelEvent implements ActionListener {
+    private MyFrame myFrame;
+    private PausePanel pausePanel;
+
+    public PausePanelEvent (PausePanel pausePanel, MyFrame myFrame) {
+        this.myFrame = myFrame;
+        this.pausePanel = pausePanel;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource().equals(pausePanel.getCon())) {
+            pausePanel.getDialog().dispose();
+        }
+        else if (e.getSource().equals(pausePanel.getRetry())) {
+            int check = myFrame.getCheck();
+
+            if (check == 0) {
+                myFrame.getContentPane().removeAll();
+                myFrame.revalidate();
+                myFrame.add(myFrame.getPauseButton());
+                myFrame.add(new GamePanel(4, 4));
+                System.out.println(check);
+            }
+            else if (check == 1) {
+                myFrame.getContentPane().removeAll();
+                myFrame.revalidate();
+                myFrame.add(myFrame.getPauseButton());
+                myFrame.add(new GamePanel(6, 6));
+                System.out.println(check);
+            }
+            else if (check == 2) {
+                myFrame.getContentPane().removeAll();
+                myFrame.revalidate();
+                myFrame.add(myFrame.getPauseButton());
+                myFrame.add(new GamePanel(8, 8));
+                System.out.println(check);
+            }
+            pausePanel.getDialog().dispose();
+            myFrame.repaint();
+            myFrame.pack();
+        }
+        else if (e.getSource().equals(pausePanel.getExits())) {
+            myFrame.getContentPane().removeAll();
+            myFrame.revalidate();
+            myFrame.add(myFrame.getMenuPanel());
+            pausePanel.getDialog().dispose();
+            myFrame.repaint();
+            myFrame.pack();
+        }
+    }
+}

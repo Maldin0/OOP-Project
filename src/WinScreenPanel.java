977a5962dc44cc05c8816697b0bddd43fb09c4a6
Sweet eyes <109953598+Bonbonbonsai@users.noreply.github.com>
2023-05-19 13:21:46 +0700

    add exit to winner screen

--- src/WinScreenPanel.java
+++ src/WinScreenPanel.java
@@ -1,14 +1,41 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class WinScreenPanel extends JFrame {
-    public JLabel winSign;
+    private JPanel mainPanel, buttonPanel;
+    private JButton exits;
+    private JLabel winSign;
 
     public WinScreenPanel() {
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+
         winSign = new JLabel();
         winSign.setHorizontalAlignment(JLabel.CENTER);
-        this.add(winSign);
+        mainPanel.add(winSign, BorderLayout.CENTER);
+
+        buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout());
+
+        exits = new JButton(new ImageIcon("image/Exit to menu button.png"));
+        exits.setPreferredSize(new Dimension(192, 64));
+        exits.addActionListener(e -> {
+            for (java.awt.Window window : java.awt.Window.getWindows()) {
+                if (window instanceof JFrame) {
+                    window.dispose();
+                }
+            }
+            new MyFrame();
+        });
+        buttonPanel.add(exits);
+
+        mainPanel.add(buttonPanel, BorderLayout.SOUTH);
 
         ImageIcon imageIcon = new ImageIcon("image/WinScreen.png");
+
+        this.add(mainPanel);
 
         this.setTitle("Image Display");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -23,5 +50,13 @@ public class WinScreenPanel extends JFrame {
 
     public static void main(String[] args) {
         new WinScreenPanel();
+    }
+
+    public JLabel getWinSign() {
+        return winSign;
+    }
+
+    public void setWinSign(JLabel winSign) {
+        this.winSign = winSign;
     }
 }
